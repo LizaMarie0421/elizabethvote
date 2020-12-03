49cac73d656c467a2ccdc25c335f9e2d30da5c48
LizaMarie <lizamariejimenez@gmail.com>
2020-12-03 12:44:52 -0500

    heroku pt 3

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::UsersController < ApplicationController
         @users = User.all
         render json: @users
     end
-
+    #melia told me to
     # GET /user/:id
     def show
         @user = User.find(params[:id])
